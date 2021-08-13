190d46011385631061d1176175cfdb0db3ba6e78
dsillman2000 <dsillman2000@gmail.com>
2021-08-13 13:53:23 -0700

    Adding EMA & BBs as treatments

--- davidplayground/utils.py
+++ davidplayground/utils.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from datetime import datetime, date, timedelta
+import numpy as np
 
 def cbquery2df(query):
     '''
@@ -31,7 +32,7 @@ def convert_timestamps(df):
 
 def add_sma(df, dt=2, cols=['open', 'close', 'high', 'low']):
     '''
-    A treatment function or adding simple moving average(s) as columns
+    A treatment function for adding simple moving average(s) as columns
     to the dataset.
     :param df: Dataframe dataset
     :param dt: Size of window for SMA
@@ -39,7 +40,41 @@ def add_sma(df, dt=2, cols=['open', 'close', 'high', 'low']):
     :return: treated dataset
     '''
     for col in cols:
-        sma_name = col + '_SMA' + str(dt)
+        sma_name = col + '_sma' + str(dt)
         sma_col = df[col].rolling(window=dt).mean()
         df[sma_name] = sma_col
+    return df
+
+def add_ema(df, dt=2, smoothing=2.0,cols=['open', 'close', 'high', 'low']):
+    '''
+    A treatment function for adding exponential moving average(s) as columns
+    to the dataset.
+    :param df: Dataframe dataset
+    :param dt: Size of window for EMA
+    :param cols: Which columns to compute EMA for
+    :return: treated dataset
+    '''
+    for col in cols:
+        ema0 = df[col][:dt].mean()
+        ema_col = [None for _ in range(dt)]
+        ema_name = col + '_ema' + str(dt)
+        for index, row in df.iterrows():
+            if index < dt:
+                continue
+            ema1 = row[col]*(smoothing/(1.0 + dt)) + ema0*(1 - smoothing/(1.0 + dt))
+            ema_col.append(ema1)
+            ema0 = ema1
+        df[ema_name] = ema_col
+    return df
+
+def add_bollinger(df, dt=2, K=2.0, middle='sma'):
+    if middle == 'sma':
+        df = add_sma(df, dt=dt, cols=['close'])
+    elif middle == 'ema':
+        df = add_ema(df, dt=dt, smoothing=2.0, cols=['close'])
+    else:
+        raise Exception('Bollinger Bands require middle method to be in ["sma", "ema"]')
+    stdev = df['close'].rolling(window=dt).std()
+    df['bb_upper'] = stdev * K + df[f'close_{middle}{dt}']
+    df['bb_lower'] = -stdev * K + df[f'close_{middle}{dt}']
     return df
